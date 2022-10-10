1f9f2061f9bebf40d3c23bb4a7acbcba824de8cc
Your Name <tranhoangloc502@gmail.com>
2022-10-10 15:03:00 +0700

    hieu ung reset

--- main2.js
+++ main2.js
@@ -7,23 +7,22 @@ function thuchien() {
     let c = document.getElementById('abc').value;
     let d ;
 
-    if (c == "1"){
+    if (c === "1"){
         d = a + b;
     }
-    if ( c == "2"){
+   else if ( c === "2"){
     d = a - b;
     }
-      if( c == "3"){
+    else  if( c === "3"){
     d = a * b;
 }
-    if ( c == "4"){
+    else {
         d = a / b;
     }
 document.getElementById('nhanketqua').innerHTML = d;
 }
 
 function myFunction() {
-    document.getElementById('s1').value = '';
-    document.getElementById('s2').value = '';
-    document.getElementById('nhanketqua').innerHTML = '';
+    document.getElementById("s1").value = "";
+    document.getElementById("s2").value = "";
 }
